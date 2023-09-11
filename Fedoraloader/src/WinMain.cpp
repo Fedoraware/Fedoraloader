c85d9f62237e46b523f01468314ee0d7516ee2fe
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-09-11 02:20:52 +0200

    Retrieve launch args

--- Fedoraloader/src/WinMain.cpp
+++ Fedoraloader/src/WinMain.cpp
@@ -1,7 +1,39 @@
 #include "Windows.h"
 
+struct LaunchInfo
+{
+	bool Silent = false;
+    bool Unprotected = false;
+    LPWSTR File = nullptr;
+};
+
+LaunchInfo GetLaunchInfo()
+{
+    int nArgs;
+    const LPWSTR* szArglist  =  CommandLineToArgvW(GetCommandLineW(), &nArgs);
+    
+	LaunchInfo info{};
+    for (int i = 0; i < nArgs; i++)
+    {
+        const auto arg = szArglist[i];
+
+	    if (wcscmp(arg, L"-silent") == 0) { info.Silent = true; continue; }
+        if (wcscmp(arg, L"-unprotected") == 0) { info.Unprotected = true; continue; }
+
+        if (wcscmp(arg, L"-file") == 0 && i < nArgs - 1)
+        {
+            i++;
+        	const auto nextArg = szArglist[i];
+            info.File = nextArg;
+        }
+    }
+
+    return info;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
-    Sleep(5000);
+    LaunchInfo launchInfo = GetLaunchInfo();
+
     return 0;
 }
