fa210e26904da2ee1d1d985201c444895f2764ac
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-09-26 20:43:27 +0200

    Applied ReSharper style

--- VAC-Bypass/src/Hooks/Hooks.h
+++ VAC-Bypass/src/Hooks/Hooks.h
@@ -2,12 +2,12 @@
 
 #include <Windows.h>
 
-HMODULE           WINAPI     Hooks_LoadLibraryExW(LPCWSTR, HANDLE, DWORD);
-HMODULE           WINAPI     Hooks_LoadLibraryExW_SteamClient(LPCWSTR, HANDLE, DWORD);
-FARPROC           WINAPI     Hooks_GetProcAddress(HMODULE, LPCSTR);
-VOID              WINAPI     Hooks_GetSystemInfo(LPSYSTEM_INFO);
-BOOL              WINAPI     Hooks_GetVersionExA(LPOSVERSIONINFOEXA);
-UINT              WINAPI     Hooks_GetSystemDirectoryW(LPWSTR, UINT);
-UINT              WINAPI     Hooks_GetWindowsDirectoryW(LPWSTR, UINT);
-DWORD             WINAPI     Hooks_GetCurrentProcessId(VOID);
-DWORD             WINAPI     Hooks_GetCurrentThreadId(VOID);
+HMODULE WINAPI Hooks_LoadLibraryExW(LPCWSTR, HANDLE, DWORD);
+HMODULE WINAPI Hooks_LoadLibraryExW_SteamClient(LPCWSTR, HANDLE, DWORD);
+FARPROC WINAPI Hooks_GetProcAddress(HMODULE, LPCSTR);
+VOID WINAPI Hooks_GetSystemInfo(LPSYSTEM_INFO);
+BOOL WINAPI Hooks_GetVersionExA(LPOSVERSIONINFOEXA);
+UINT WINAPI Hooks_GetSystemDirectoryW(LPWSTR, UINT);
+UINT WINAPI Hooks_GetWindowsDirectoryW(LPWSTR, UINT);
+DWORD WINAPI Hooks_GetCurrentProcessId(VOID);
+DWORD WINAPI Hooks_GetCurrentThreadId(VOID);
