63d02e2b313556f09e45ed19e4a8a2437560e8e0
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-10-02 02:52:56 +0200

    Added Windows 11 support

--- Fedoraloader/Fedoraloader/src/Loader/Injector/ManualMap/Native/Native.cpp
+++ Fedoraloader/Fedoraloader/src/Loader/Injector/ManualMap/Native/Native.cpp
@@ -6,18 +6,18 @@
 #include "../../../../Utils/Utils.h"
 #include "../../../../Utils/Pattern/Pattern.h"
 
-// 6.1.7601: \x8B\xFF\x55\x8B\xEC\x56\x68
-
 using namespace std::string_literals;
-
-const std::vector IIFT_Patterns = {
-	"\x8B\xFF\x55\x8B\xEC\x83\xEC\x00\x53\x56\x57\x8D\x45\x00\x8B\xFA"s, // 10.0.19041.3393
-	"\x8B\xFF\x55\x8B\xEC\x83\xEC\x00\x8D\x45\x00\x53\x56\x57\x8B\xDA"s, // 10.0.10240
-};
 
 PBYTE InitRtlInsertInvertedFunctionTable()
 {
-	for (const auto& pattern : IIFT_Patterns)
+	static const std::vector PATTERNS = {
+		"\x8B\xFF\x55\x8B\xEC\x83\xEC\x00\x53\x56\x57\x8D\x45\x00\x8B\xFA\x50\x8D\x55"s,	// 10.0.22621.1485
+		"\x8B\xFF\x55\x8B\xEC\x83\xEC\x00\x53\x56\x57\x8D\x45\x00\x8B\xFA"s,				// 10.0.19041.3393
+		"\x8B\xFF\x55\x8B\xEC\x83\xEC\x00\x8D\x45\x00\x53\x56\x57\x8B\xDA"s,				// 10.0.10240
+		//"\x8B\xFF\x55\x8B\xEC\x56\x68"s, // 6.1.7601
+	};
+
+	for (const auto& pattern : PATTERNS)
 	{
 		if (const PBYTE result = Pattern::Find("ntdll.dll", pattern))
 		{
@@ -29,8 +29,8 @@ PBYTE InitRtlInsertInvertedFunctionTable()
 	{
 		DWORD major, minor, buildNubmer;
 		Utils::GetVersionNumbers(&major, &minor, &buildNubmer);
-		const auto msg = std::format("Windows Version not supported:\n{:d}.{:d}.{:d}", major, minor, buildNubmer);
-		MessageBoxA(nullptr, msg.c_str(), "Error", MB_OK | MB_ICONERROR);
+		const auto msg = std::format("Windows version not supported: {:d}.{:d}.{:d}\nPlease report this incident.", major, minor, buildNubmer);
+		MessageBoxA(nullptr, msg.c_str(), "Warning", MB_OK | MB_ICONWARNING);
 	}
 #endif
 
